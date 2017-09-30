a7b5df575dc32e42b4f2e9b1d63ff0bcb81b2a28
Maximilian Karacsony <maximilian.karacsony@mail.de>
2017-09-30 16:39:05 +0200

    Improve seeding by deleting seed log on every startup to prevent seeding from not being executed

--- bin/seed.js
+++ bin/seed.js
@@ -3,13 +3,16 @@ const path = require('path');
 
 const models = require('../models');
 const userService = require('../services/userService');
+const { removeFileIfExists } = require('../utils');
 
 const seedDirectoryPath = path.resolve(__dirname, '../seeds');
 const seedLogPath = path.join(seedDirectoryPath, 'seed-log.json');
 
 const umzug = new Umzug({
   storage: 'json',
-  path: seedLogPath,
+  storageOptions: {
+    path: seedLogPath,
+  },
   // see: https://github.com/sequelize/umzug/issues/17
   migrations: {
     params: [{
@@ -24,5 +27,6 @@ const umzug = new Umzug({
 });
 
 models.sequelize.sync({ force: true })
+  .then(() => removeFileIfExists(seedLogPath))
   .then(() => umzug.up())
   .then(() => process.exit(0));
